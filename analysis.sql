074d4ec0c4d1b15100f5b612e4334daf69e9214c
Jeban Christo R <chrischristo200@gmail.com>
2025-07-20 18:25:29 +0530

    Sql analysis by Unique tracks listened

--- analysis.sql
+++ analysis.sql
@@ -6,3 +6,6 @@ select * from spotify_analysis;
 select count(*) from spotify_analysis;
 select track_name,artist_name ,count(artist_name) from spotify_analysis group by track_name,artist_name;
 select * from spotify_analysis where artist_name regexp "^[A-Za-z ]+$";
+SELECT artist_name, COUNT(DISTINCT track_name) AS unique_tracks, COUNT(*) AS total_plays
+FROM spotify_analysis
+GROUP BY artist_name;
